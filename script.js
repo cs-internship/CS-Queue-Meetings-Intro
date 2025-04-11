0d32652ded845d6fc18ce72138d66584dd4e7190
Ali Sadeghi <ali909392@gmail.com>
2025-04-11 18:18:58 +0330

    Remove fullscreen button and related functionality; adjust special mode behavior

--- script.js
+++ script.js
@@ -26,35 +26,15 @@ function animate() {
     window.requestAnimationFrame(animate);
 }
 
-window.requestAnimationFrame(animate);
-
-const fullscreenBtn = document.querySelector(".fullscreen-btn");
-
-fullscreenBtn.addEventListener("click", () => {
-    document.documentElement.requestFullscreen();
-});
-
-document.addEventListener("fullscreenchange", () => {
-    if (document.fullscreenElement) {
-        fullscreenBtn.style.display = "none";
-    } else {
-        fullscreenBtn.style.display = "flex";
-    }
-});
-
 const specialModeCheckbox = document.querySelector(".special-mode-checkbox");
 
 specialModeCheckbox.addEventListener("change", () => {
     if (specialModeCheckbox.checked) {
+        window.requestAnimationFrame(animate);
         dvd.style.display = "block";
-        if (!document.fullscreenElement) {
-            fullscreenBtn.style.display = "flex";
-        }
     } else {
         dvd.style.display = "none";
-        fullscreenBtn.style.display = "none";
     }
 });
 
 dvd.style.display = "none";
-fullscreenBtn.style.display = "none";
