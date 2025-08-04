5acfb167647f354cb14e946a83176ee63d78d606
Ali Sadeghi <ali909392@gmail.com>
2025-08-04 21:19:11 +0330

    Enhance meeting settings and UI: add time input with Persian numeral conversion, update modal styles, and improve button interactions.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let dvd = document.querySelector(".dvd-csi");
 const dvdWidth = dvd.clientWidth;
 const dvdHeight = dvd.clientHeight;
 
-function animate() {
+const animate = () => {
     const screenHeight = document.body.clientHeight;
     const screenWidth = document.body.clientWidth;
 
@@ -63,10 +63,42 @@ interviewModeCheckbox.addEventListener("change", () => {
     }
 });
 
+const persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+
+const convertENtoFA = (input) => {
+    if (input) {
+        let output = input.toString().replace(/\d/g, (digit) => {
+            return persianNumbers[parseInt(digit, 10)];
+        });
+
+        return output;
+    } else {
+        return "";
+    }
+};
+
+const timeInput = document.querySelector(".meeting-time-input");
+const meetingTime = document.querySelector(".meeting-time");
+
+let hour, minute;
+
+const updateTimeFromInput = () => {
+    const timeValue = timeInput.value;
+    [hour, minute] = timeValue.split(":");
+
+    meetingTime.innerText = `${convertENtoFA(hour)}:${convertENtoFA(minute)}`;
+};
+
+updateTimeFromInput();
+
+timeInput.addEventListener("input", () => {
+    updateTimeFromInput();
+});
+
 const updateCountdown = () => {
     const now = new Date();
     const target = new Date();
-    target.setHours(18, 0, 0, 0);
+    target.setHours(hour, minute, 0, 0);
 
     const countdownEl = document.querySelector(".countdown");
 
@@ -93,23 +125,43 @@ updateCountdown();
 setInterval(updateCountdown, 1000);
 
 let settingClicked = 0;
-const settingMenu = document.querySelector(".setting-btn");
+const settingBtn = document.querySelector(".setting-btn");
 const settingModal = document.querySelector(".setting-modal");
 
-settingMenu.addEventListener("click", () => {
+const handleOutsideClick = (event) => {
+    if (
+        !settingModal.contains(event.target) &&
+        !settingBtn.contains(event.target)
+    ) {
+        closeSettingModal();
+        settingClicked++;
+        settingBtn.style.transform = `rotate(${settingClicked * 90}deg)`;
+    }
+};
+
+const closeSettingModal = () => {
+    settingModal.classList.remove("show");
+
+    setTimeout(() => {
+        settingModal.style.display = "none";
+    }, 300);
+    
+    document.removeEventListener("click", handleOutsideClick);
+};
+
+settingBtn.addEventListener("click", () => {
     if (settingClicked++ % 2 == 0) {
         settingModal.style.display = "flex";
         setTimeout(() => {
             settingModal.classList.add("show");
         }, 0);
+
+        document.addEventListener("click", handleOutsideClick);
     } else {
-        settingModal.classList.remove("show");
-        setTimeout(() => {
-            settingModal.style.display = "none";
-        }, 300);
+        closeSettingModal();
     }
 
-    settingMenu.style.transform = `rotate(${settingClicked * 90}deg)`;
+    settingBtn.style.transform = `rotate(${settingClicked * 90}deg)`;
 });
 
 const ICARUSs = document.querySelectorAll(".ICARUS");
